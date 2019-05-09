9ce2f00a4a11cd032b2038388c4b96b1be3194f4
Christoph <christoph@christoph-Aspire-A515-51G>
2019-05-09 20:31:11 +0200

    D3js changes + KotlinConverter changes
    D3js
     - dynamic text size and circle radius
     - collision prevention
    Kotlin
    - almost working (still some empty nodes)

--- SpiegelConverterJson/src/main/kotlin/converter.kt
+++ SpiegelConverterJson/src/main/kotlin/converter.kt
@@ -7,7 +7,7 @@ import kotlin.streams.toList
 
 fun main(){
 
-    val reader: BufferedReader = File("/home/christoph/Desktop/HCI Data/tags-small.json").bufferedReader()
+    val reader: BufferedReader = File("/home/christoph/Desktop/HCI Data/tags.json").bufferedReader()
 //    val readNodes: BufferedReader = File("/home/christoph/Desktop/HCI Data/nodes.json").bufferedReader()
 //    val writeNodes: PrintStream = PrintStream("/home/christoph/Desktop/HCI Data/nodes.json");
 //    val readLinks: BufferedReader = File("/home/christoph/Desktop/HCI Data/links.json").bufferedReader()
@@ -18,11 +18,11 @@ fun main(){
     val articles: List<Article> = gson.fromJson(inputString, object: TypeToken<List<Article>>() {}.type)
 
 //    println("calcBasicTags: " + measureTimeMillis { calcBasicTags(articles) })
-    val baseTags: Set<String> = calcBasicTags()
+    val baseTags: Set<String> = calcBaseTags()
 //    println("calcUniqueTags: " + measureTimeMillis { calcUniqueTags(articles) })
     val uniqueTags = calcUniqueTags(articles)
 
-
+//    saving nodes and links in files for less operations
 //    val nodesString: String = readNodes.use { it.readText() }
 //    val nodes: List<Node> = gson.fromJson(nodesString, object: TypeToken<List<Node>>() {}.type)
 //    val linksString: String = readLinks.use { it.readText() }
@@ -31,7 +31,7 @@ fun main(){
 //    println(links)
 
 //    println("calcNodes: " + measureTimeMillis { calcNodes(articles, uniqueTags) })
-    val nodes: List<Node> = calcNodes(articles, uniqueTags)
+    val nodes: List<Node> = calcNodes(articles, uniqueTags).sortedBy { node -> node.id }
 //    writeNodes.print(gson.toJson(nodes))
 //    println("calcLinks: " + measureTimeMillis { calcLinks(articles, uniqueTags) })
     val links: List<Link> = calcLinks(articles, uniqueTags)
@@ -46,35 +46,40 @@ fun main(){
 //    println("setGroupsToNodes: " + measureTimeMillis { setGroupsToNodes(nodes, baseTags, groups, twentyPercentLinks) })
     setGroupsToNodes(nodes, baseTags, groups, twentyPercentLinks)
 //    println("cleanNodes: " + measureTimeMillis { cleanNodes(nodes, twentyPercentLinks) })
-    val cleanedNodes = cleanNodes(nodes, twentyPercentLinks)
+    val cleanedNodes: List<Node> = cleanNodes(nodes, twentyPercentLinks)
 //    println("cleanLinks: " + measureTimeMillis { cleanLinks(twentyPercentLinks) })
-    val cleanedLinks = cleanLinks(twentyPercentLinks)
+    val cleanedLinks: List<Link> = cleanLinks(twentyPercentLinks, cleanedNodes)
+
+    cleanedLinks.forEach { link ->
+        var nodeFound: Boolean = false;
+        for(node in cleanedNodes){
+            if (node.id == link.source)
+                nodeFound = true
+        }
+        if(!nodeFound)
+            println("missing node for $link")
+    }
+
+    cleanedNodes.forEach { node ->
+        var linkFound: Boolean = false;
+        for(link in cleanedLinks){
+            if(link.source == node.id || link.target == node.id)
+                linkFound = true
+        }
+        if(!linkFound)
+            println("missing link for $node")
+    }
 
     val s: String = gson.toJson(NodeLink(cleanedNodes, cleanedLinks))
     val printStream = PrintStream("/home/christoph/Desktop/HCI Data/formattedData.json")
     printStream.print(s)
-
-//    var links: List<Link> = calcLinks(uniqueTags, articles)
-//    var nodes: List<Node> = calcNodes(articles, uniqueTags)
-//
-//    var groupedLinks: Map<String, List<Link>> = links.stream()
-//        .collect(Collectors.groupingBy { l -> l.source })
-//
-//    var nodes: MutableList<Node> = ArrayList()
-//    uniqueTags
-//        .filter { t -> links.any { l -> l.source == t || l.target == t } }
-//        .forEach { t -> nodes.add(Node(t, if (groupedLinks[t] == null) 0 else groupedLinks[t]!!.size)) }
-//
-//    var s: String = gson.toJson(NodeLink(nodes, links))
-//    val printStream: PrintStream = PrintStream("/home/christoph/Desktop/formattedData.json")
-//    printStream.print(s)
 }
 
 
 /**
  * Calculates the basic tags that were used to find the articles.
  */
-fun calcBasicTags(): Set<String> {
+fun calcBaseTags(): Set<String> {
     return "Politik, Meinung, Wirtschaft, Panorama, Sport, Kultur, Netzwelt, Wissenschaft, Gesundheit, Reise"
             .split(",")
             .map { t -> t.trim() }
@@ -92,7 +97,9 @@ fun calcUniqueTags(articles: List<Article>): Set<String> {
 }
 
 fun calcNodes(articles: List<Article>, uniqueTags: Set<String>): List<Node> {
-    return uniqueTags.map{ tag ->
+    return uniqueTags
+        .filter { tag -> tag.isNotBlank() }
+        .map{ tag ->
         val node = Node(tag)
         node.nrArticles = articles.count { a -> a.tags.contains(tag) }
         node
@@ -157,22 +164,24 @@ private fun setGroupsToNodes(
     }
 }
 
-private fun cleanLinks(twentyPercentLinks: List<Link>): List<Link> {
-    return twentyPercentLinks.
-        filter { link ->
-            link.source.isNotBlank() && link.target.isNotBlank()
-    }
-}
-
 private fun cleanNodes(
     nodes: List<Node>,
     twentyPercentLinks: List<Link>
 ): List<Node> {
     return nodes
+//        .filter { node -> node.group > 0 }
         .filter { node ->
-//            node.group != 0 &&
             twentyPercentLinks.any { link -> link.source == node.id || link.target == node.id }
         }
+}
+
+private fun cleanLinks(twentyPercentLinks: List<Link>,
+                       cleanNodes: List<Node>): List<Link> {
+    return twentyPercentLinks.
+        filter { link ->
+            link.source.isNotBlank() && link.target.isNotBlank()
+        }
+        .filter { link -> cleanNodes.any { node -> node.id == link.source } }
 }
 
 
@@ -187,48 +196,3 @@ data class Node (var id: String, var group: Int = 0, var nrArticles: Int = 0)
 data class NodeLink (var nodes: List<Node>, var links: List<Link>)
 
 data class Group (var id: Int = 0, var name: String = "")
-
-
-
-//var groupedLinks: Map<String, List<Link>> = links.stream()
-//    .collect(Collectors.groupingBy { l -> l.source })
-//
-//fun calcGroupedLinks
-
-//val calcIt: (List<Article>) -> (Set<String>) = {
-//    val uniqueTags: MutableSet<String> = mutableSetOf()
-//    it.forEach {
-//        it.tags.split(",").forEach({
-//            uniqueTags.add(it.trim())
-//        })
-//    }
-//    uniqueTags
-//}
-
-//fun calcLinks(uniqueTags: Set<String>, articles: List<Article>): List<Link> {
-//    var links: ArrayList<Link> = ArrayList()
-//    uniqueTags
-//        .filter { tag -> tag != "" } // remove empty entries
-//        .forEach { tag ->
-//            // iterate over every tag
-//            // find all articles, where the tag is included
-//            val filteredArticles = articles.filter { art -> art.tags.contains(tag) }
-//
-//            val map: MutableMap<String, Int> = mutableMapOf()
-//            for(art in filteredArticles){
-//                val remainingTags: List<String> = art.tags.split(",").filter { t -> t.trim() != tag }.map { t -> t.trim() }
-//                remainingTags.forEach { t ->
-//                    // count the amount of articles containing the current tag
-//                    if (map[t] != null) { // increment counter
-//                        map[t] = map[t]!! + 1
-//                    } else { // amount = 1 if no map is existing yet
-//                        map[t] = 1
-//                    }
-//                }
-//            }
-//            map
-//                .filter { (target, value) -> value > 10 }
-//                .forEach { (target, value) -> links.add(Link(tag, target, value))}
-//        }
-//    return links
-//}
