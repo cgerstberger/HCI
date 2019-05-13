740514777b101800e83710ee35490cdea96850cd
Restoro <patrick.fritsch@hotmail.com>
2019-05-13 21:55:06 +0200

    Added highlight, grouping, search

--- HCI/script.js
+++ HCI/script.js
@@ -5,25 +5,68 @@ height = +svg.node().getBoundingClientRect().height
 
 svg = svg.call(d3.zoom().on("zoom", function () {
   svg.attr("transform", d3.event.transform)
-}))
+})).on("dblclick.zoom", null)
   .append("g")
 
 //Load Data
-d3.json("./formattedData_07.json", function (error, _graph) {
+d3.json("./formattedData_08.json", function (error, _graph) {
   if (error) throw error;
 
-  graph = _graph
-
   //Sort for correct rendering order
-  graph.nodes.sort(function (a, b) {
+  _graph.nodes.sort(function (a, b) {
     return a.nrArticles - b.nrArticles;
   });
 
+  graph = _graph
+
+  groupTags = ["", "Kultur", "Netzwelt", "Sport", "Panorama", "Politik", "Meinung", "Wirtschaft", "Gesundheit", "Reise", "Wissenschaft"]
+  nodeById = {};
+  groupNodes = {};
+  groupedNodes = {};
+  linkNodeSources = {};
+  linkNodeTargets = {};
+
+  graph.nodes.forEach(n => {
+    nodeById[n.id] = n;
+    if (!(n.group in groupNodes))
+      groupNodes[n.group] = [];
+
+    groupNodes[n.group].push(n);
+  });
+
+  graph.links.forEach(l => {
+    sourceNode = nodeById[l.source]
+    l.prevSource = sourceNode;
+    if (!(sourceNode.id in linkNodeSources))
+      linkNodeSources[sourceNode.id] = [];
+
+    linkNodeSources[sourceNode.id].push(l);
+
+    targetNode = nodeById[l.target]
+    l.prevTarget = targetNode;
+    if (!(targetNode.id in linkNodeTargets))
+      linkNodeTargets[targetNode.id] = [];
+
+    linkNodeTargets[targetNode.id].push(l);
+  })
+
+  console.log(nodeById)
+
+  //Init fuzy search
+  initFuzy();
   //Init elements to display
   initDisplay();
   //Init simulation elements
   initSimulation();
 });
+
+function testCopy(src) {
+  return Object.assign({}, src);
+}
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+var currentHighlight = undefined;
+
 
 
 ////////// FORCE SIMULATION ////////// 
@@ -69,7 +112,11 @@ forceProperties = {
     dynamicDistance: false,
     distance: 30,
     iterations: 1
-  }
+  },
+  highlight: {
+    nodeOpacity: 0.3,
+    linkOpacity: 0.1
+  },
 }
 
 function initForces() {
@@ -104,34 +151,58 @@ function updateForces() {
 
 ////////// DISPLAY //////////
 function initDisplay() {
-  var color = d3.scaleOrdinal(d3.schemeCategory20);
-
   link = svg.append("g")
     .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-    .attr("stroke-width", function (d) { return Math.sqrt(d.value); });
+    .selectAll(".link")
 
   node = svg.append("g")
     .attr("class", "nodes")
-    .selectAll("g")
-    .data(graph.nodes)
-    .enter().append("g")
+    .selectAll(".node")
 
-  circles = node.append("circle")
+
+  update();
+  updateDisplay();
+}
+
+function update() {
+  link = link.data(graph.links, function (d) { return d.source.id + "-" + d.target.id; });
+  link.exit().remove();
+  link = link.enter().append("line")
+    .attr("stroke-width", function (d) { return Math.sqrt(d.value * 2); })
+    .merge(link);
+
+  node = svg.select(".nodes").selectAll("g").data(graph.nodes, function (d) { return d.id; });
+
+  var nodeExit = node.exit().remove();
+
+  nodeEnter = node.enter().append("g").merge(node);
+
+  circles = nodeEnter.append("circle")
     .attr("r", function (d) { return Math.sqrt(d.nrArticles * 3) + 2 })
     .attr("fill", function (d) { return color(d.group); })
 
     .call(d3.drag()
       .on("start", dragstarted)
       .on("drag", dragged)
-      .on("end", dragended));
+      .on("end", dragended))
+    .on("click", function (d) {
+      // if double click timer is active, this click is the double click
+      if (dblclick_timer) {
+        clearTimeout(dblclick_timer)
+        dblclick_timer = false
+        removeGroup(d)
+      }
+      // otherwise, what to do after single click (double click has timed out)
+      else dblclick_timer = setTimeout(function () {
+        dblclick_timer = false
+        highlight(d)
+      }, 250)
+    })
 
-  node.append("title")
+  nodeEnter.append("title")
     .text(function (d) { return d.id; });
 
-  lables = node.append("text")
+  lables = nodeEnter.append("text")
     .text(function (d) {
       return d.id;
     })
@@ -140,15 +211,15 @@ function initDisplay() {
     .attr("dy", ".35em")
     .style('font-size', function (d) { return Math.sqrt(d.nrArticles); });
 
-  updateDisplay();
+  simulation.nodes(graph.nodes)
+  simulation.alpha(1).restart();
 }
 
-function updateDisplay() {
-  node
-    .attr("r", forceProperties.collide.radius)
+dblclick_timer = false
 
+function updateDisplay() {
   link
-    .attr("opacity", forceProperties.link.enabled ? 1 : 0);
+    .style("opacity", forceProperties.link.enabled ? 1 : 0);
 
 }
 function ticked() {
@@ -158,7 +229,7 @@ function ticked() {
     .attr("x2", function (d) { return d.target.x; })
     .attr("y2", function (d) { return d.target.y; });
 
-  node
+  nodeEnter
     .attr("transform", function (d) {
       return "translate(" + d.x + "," + d.y + ")";
     });
@@ -193,6 +264,141 @@ function updateAll() {
   updateForces();
   updateDisplay();
 }
+
+function isConnected(d, a) {
+  return d.id === a.id || d.id in linkNodeSources && linkNodeSources[d.id].some(l => l.target.id === a.id) || d.id in linkNodeTargets && linkNodeTargets[d.id].some(l => l.source.id === a.id);
+}
+
+function updateHighlight() {
+  nodeEnter.style('opacity', (n) => {
+    return isConnected(currentHighlight, n) ? 1 : forceProperties.highlight.nodeOpacity;
+  });
+  link.style('opacity', l => (l.source === currentHighlight || l.target === currentHighlight) ? 1 : forceProperties.highlight.linkOpacity);
+}
+
+function search(input) {
+  input = document.getElementById("searchField").value;
+  currentHighlight = null;
+  if(input) {
+    var searchResult = fuse.search(input);
+    console.log(searchResult);
+    nodeEnter.style('opacity', (n) => {
+      return searchResult.includes(n) ? 1 : forceProperties.highlight.nodeOpacity;
+    });
+    link.style('opacity', 0.1);
+  } else {
+    nodeEnter.style('opacity', 1);
+    link.style('opacity', 1);
+  }
+}
+
+function highlight(d) {
+  console.log(linkNodeSources[d.id])
+  if (d === currentHighlight) {
+    nodeEnter.style('opacity', 1);
+    link.style('opacity', 1);
+    currentHighlight = null;
+  } else {
+    nodeEnter.style('opacity', (n) => {
+      return isConnected(d, n) ? 1 : forceProperties.highlight.nodeOpacity;
+    });
+    link.style('opacity', l => (l.source === d || l.target === d) ? 1 : forceProperties.highlight.linkOpacity);
+    currentHighlight = d;
+  }
+}
+
+function removeGroup(d) {
+  nodeEnter.style('opacity', 1);
+  link.style('opacity', 1);
+  currentHighlight = null;
+  
+  if (d.group in groupedNodes) {
+    groupedGraphNode = groupedNodes[d.group]
+    groupNodes[d.group].forEach(n => {
+      n.x = groupedGraphNode.x + Math.abs(n.x - groupedGraphNode.oldX);
+      n.y = groupedGraphNode.y + Math.abs(n.y - groupedGraphNode.oldY);
+    });
+
+    graph.nodes.push(...groupNodes[d.group])
+
+    graph.links.filter(l => l.source == groupedGraphNode).forEach(l => {
+      l.source = l.prevSource;
+    })
+    graph.links.filter(l => l.target == groupedGraphNode).forEach(l => {
+      l.target = l.prevTarget;
+    })
+
+    delete linkNodeSources[groupedGraphNode.id]
+    delete linkNodeTargets[groupedGraphNode.id] 
+
+    graph.nodes.splice(graph.nodes.indexOf(groupedGraphNode), 1);
+    delete groupedNodes[d.group]
+  } else {
+    console.log(d.group)
+    let newGroupGraph = { id: "." + groupTags[d.group], group: d.group, nrArticles: 0, vx: 0, xy: 0, x: 500, y: 500 };
+    let sumArticles = 0;
+    let centerX = 0;
+    let centerY = 0;
+
+    linkNodeSources[newGroupGraph.id] = []
+    linkNodeTargets[newGroupGraph.id] = []
+    groupNodes[d.group].forEach(n => {
+      centerX += n.x;
+      centerY += n.y;
+      sumArticles += n.nrArticles;
+
+      if (n.id in linkNodeSources)
+        linkNodeSources[n.id].forEach(l => {
+          l.source = newGroupGraph;
+          linkNodeSources[newGroupGraph.id].push(l);
+        });
+
+      if (n.id in linkNodeTargets)
+        linkNodeTargets[n.id].forEach(l => {
+          l.target = newGroupGraph;
+          linkNodeTargets[newGroupGraph.id].push(l);
+        });
+    });
+
+    console.log(linkNodeTargets[newGroupGraph.id])
+
+    newGroupGraph.x = centerX / groupNodes[d.group].length;
+    newGroupGraph.y = centerY / groupNodes[d.group].length;
+    newGroupGraph.nrArticles = sumArticles;
+    newGroupGraph.oldX = newGroupGraph.x
+    newGroupGraph.oldY = newGroupGraph.y
+
+    let group = graph.nodes.filter(n => n.group !== d.group);
+
+    group.push(newGroupGraph);
+
+    graph.nodes = group.sort(function (a, b) {
+      return a.nrArticles - b.nrArticles;
+    });
+
+    groupedNodes[d.group] = newGroupGraph;
+  }
+
+  update();
+}
+
+///////// FUZY SEARCH ////////// 
+function initFuzy() {
+  var options = {
+    shouldSort: true,
+    threshold: 0.2,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 2,
+    keys: [
+      "id"
+    ]
+  };
+  fuse = new Fuse(graph.nodes, options); 
+}
+
+
 
 
 
